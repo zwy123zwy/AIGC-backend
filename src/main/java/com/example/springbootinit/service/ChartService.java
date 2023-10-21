461d4a77892455f88ee8e540e746322c7e4795f2
zwy123zwy <81016312+zwy123zwy@users.noreply.github.com>
2023-10-21 09:48:02 +0800

    AIGC项目后端提交

--- src/main/java/com/example/springbootinit/service/ChartService.java
+++ src/main/java/com/example/springbootinit/service/ChartService.java
@@ -5,9 +5,7 @@ import com.baomidou.mybatisplus.extension.service.IService;
 import com.example.springbootinit.model.entity.Chart;
 
 /**
-* @author Zhangwenye
-* @description 针对表【chart(图表信息表)】的数据库操作Service
-* @createDate 2023-08-02 16:46:24
+
 */
 public interface ChartService extends IService<Chart> {
 
